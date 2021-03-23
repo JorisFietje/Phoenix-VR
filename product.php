238264dd8aedd0f6a8ac7a31b0125363226a3b5a
Thabit030 <57993677+Thabit030@users.noreply.github.com>
2021-03-23 13:37:07 +0100

    Update product.php

--- product.php
+++ product.php
@@ -29,12 +29,15 @@
             </a>
           </div>
 
+  <br>
   <h2>VR Brillen:<br></h2>
   <p>
   <p>Met een VR-bril duik je in de wondere wereld van Virtual Reality! </p>
   <p>Zet een VR-bril op je hoofd en geniet van de mooiste VR-applicaties en 360-graden video's.
     Kies voor een VR-bril die je op een PC aansluit voor extra power, of voor een standalone VR-bril om te genieten van extra vrijheid zonder kabels.
   </p>
+  <br>
+
 
   <div class="container">
     <div class="row">
@@ -43,7 +46,7 @@
         <img src="image/oculusgo.png" class="card-img-top">
         <div class="card-body">
           <h5 class="card-title">Oculus Go 32GB</h5>
-          <p class="card-text">ExLibris: Voor het specifieke doel waarvoor ik de Oculus Go in wil zetten (eenvoudige VR-ervaring in een museum middels een 360 graden filmpje)
+          <p class="card-text">Voor het specifieke doel waarvoor ik de Oculus Go in wil zetten (eenvoudige VR-ervaring in een museum middels een 360 graden filmpje)
             voldoet de headset in veel opzichten uitstekend. De prijs/kwaliteitsverhouding is ook dik in orde. </p>
             <span class="price">€ 329,47</span>
             <a href="#" class="btn btn-primary">Bekijk</a> <a href="#"><i class="fas fa-shopping-cart"></i></a>
@@ -51,6 +54,156 @@
       </div>
 
       <div class="card" style="width: 18rem;">
+        <img src="image/oculusrifts.png" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">Oculus Rift S</h5>
+          <p class="card-text">Goedkoop (relatief), scherp zat en met een (powered) usb hub (of makkelijk omruilen via coolblue, zeker binnen 30 dagen) is de setup snel en makkelijk. Tracking is prima overigens, behalve bij langdurig achter de rug zwaaien met de controllers, al weet ik niet waarom je dat ooit zou doen.</p>
+            <span class="price">€ 342,50,-</span>
+            <a href="#" class="btn btn-primary">Bekijk</a> <a href="#"><i class="fas fa-shopping-cart"></i></a>
+        </div>
+      </div>
+
+      <div class="card" style="width: 18rem;">
+        <img src="image/oculusquest2.png" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">Oculus Quest 2</h5>
+          <p class="card-text">De Quest 2 is een standalone-VR-headset die optioneel ook met een pc kan worden gekoppeld, voor gebruik met VR-apps voor de pc. Ten opzichte van zijn voorganger biedt de Quest 2 een snellere processor, een beeldscherm met een twee keer hogere resolutie etc.</p>
+            <span class="price">€ 349,-</span>
+            <a href="#" class="btn btn-primary">Bekijk</a> <a href="#"><i class="fas fa-shopping-cart"></i></a>
+        </div>
+      </div>
+
+      <div class="card" style="width: 18rem;">
+        <img src="image/samsungnewgear.png" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">Samsung New Gear VR + Gear VR Controller</h5>
+          <p class="card-text">Je in een andere wereld waden was nog nooit zo eenvoudig. De GEAR VR 2017 verbaasd me enorm met het feit dat een smartphone zulke prestaties kan leveren, prachtig beeld, zeer snelle response tijden.</p>
+            <span class="price">€ 199,99,-</span>
+            <a href="#" class="btn btn-primary">Bekijk</a> <a href="#"><i class="fas fa-shopping-cart"></i></a>
+        </div>
+      </div>
+
+      <div class="card" style="width: 18rem;">
+        <img src="image/oculusrift.png" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">Oculus Rift</h5>
+          <p class="card-text">Tijdens de summer of rift de set aangeschafd voor 450 EUR. Dat is deze set zeker waard. De immersie die het geeft is overweldigend. De touch controllers geven zeer goed aan waar je je bevind in de wereld.</p>
+            <span class="price">€ 618,92,-</span>
+            <a href="#" class="btn btn-primary">Bekijk</a> <a href="#"><i class="fas fa-shopping-cart"></i></a>
+        </div>
+      </div>
+
+      <div class="card" style="width: 18rem;">
+        <img src="image/oculusquest128gb.jpeg" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">Oculus Quest 128GB</h5>
+          <p class="card-text">De Quest is volgens mij, bijna, de VR headset voor de massa, het feit dat je hem gewoon moet opzetten en kan beginnen gamen of streamen is een dikke meerwaarde tegenover andere VR headsets. 100% standalone, batteries and sound included.</p>
+            <span class="price">€ 558,23 -</span>
+            <a href="#" class="btn btn-primary">Bekijk</a> <a href="#"><i class="fas fa-shopping-cart"></i></a>
+        </div>
+      </div>
+
+      <div class="card" style="width: 18rem;">
+        <img src="image/hp.jpeg" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">HP Windows Mixed Reality headset</h5>
+          <p class="card-text">Vooruitgang tegen over een dk2, maar dat is logisch. Nadeel zijn de kijkhoeken, rechtdoor is top zijkant is wazig. Ik ben hier wel snel aan gewend trouwens, is ook niet gek in een virtuele wereld. Ik raadt WMR aan,maar je moet er niet teveel voor betalen. Leuk voor weinig dus.</p>
+            <span class="price">€ 299,-</span>
+            <a href="#" class="btn btn-primary">Bekijk</a> <a href="#"><i class="fas fa-shopping-cart"></i></a>
+        </div>
+      </div>
+
+      <div class="card" style="width: 18rem;">
+        <img src="image/primax.jpeg" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">Pimax 5K Plus</h5>
+          <p class="card-text">Fenomenale kijkervaring. De extra brede kijkhoek vermindert erg het screen door effect dat je met headsets zoals de Vive nog wel eens ziet, al zijn games er niet altijd op voorbereid. In de ooghoeken zal het er dus soms ietwat vreemd uitzien, maar dit valt vaak eenvoudig te negeren.</p>
+            <span class="price">€ 689,-</span>
+            <a href="#" class="btn btn-primary">Bekijk</a> <a href="#"><i class="fas fa-shopping-cart"></i></a>
+        </div>
+      </div>
+
+      <div class="card" style="width: 18rem;">
+        <img src="image/htcvive.png" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">HTC Vive Cosmos Elite</h5>
+          <p class="card-text">Prima en indrukwekkend apparaat dat ik bijna dagelijks gebruik om op een inspannende manier te ontspannen, na een dag achter het scherm op m'n werk. Ik vind dat er veel leuke (en gratis) content te vinden is op Steam, en bijna alles werkt prima. Gebruik i.c.m. 2080Ti.</p>
+            <span class="price">€ 1299,-</span>
+            <a href="#" class="btn btn-primary">Bekijk</a> <a href="#"><i class="fas fa-shopping-cart"></i></a>
+        </div>
+      </div>
+
+      <div class="card" style="width: 18rem;">
+        <img src="image/sony.png" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">	Sony PlayStation VR</h5>
+          <p class="card-text">Na een paar jaar wachten uiteindelijk toch besloten om de PS VR V2 headset te halen. De headset komt vanwege een aantal uitstekende games zeer goed tot zijn recht en laat zien dat VR een grootse plek in de gamingtoekomst zal hebben. Ik kijk al uit naar de PS5 opvolger!</p>
+            <span class="price">€ 158,-</span>
+            <a href="#" class="btn btn-primary">Bekijk</a> <a href="#"><i class="fas fa-shopping-cart"></i></a>
+        </div>
+      </div>
+
+      <div class="card" style="width: 18rem;">
+        <img src="image/vrbox.jpeg" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">VR BOX VR-bril</h5>
+          <p class="card-text">Beleef jouw favoriete speelfilm of videogame nog intenser met de VR BOX Virtual Reality Bril. Met de VR BOX Virtual Reality bril krijg je het gevoel onderdeel te zijn van je eigen virtuele wereld. Je plaatst het toestel in de VR BOX smartphone houder en jou telefoon wordt omgetoverd tot een eigen 3D bioscoop.</p>
+            <span class="price">€ 17,50 -</span>
+            <a href="#" class="btn btn-primary">Bekijk</a> <a href="#"><i class="fas fa-shopping-cart"></i></a>
+        </div>
+      </div>
+
+      <div class="card" style="width: 18rem;">
+        <img src="image/visor.png" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">Dell Visor</h5>
+          <p class="card-text">Dive into mind-bending reality Dell systems are the foundation of virtual reality, delivering immersive experiences via innovative machines packed with top-tier specs. We've created a set of standards and collaborated with industry leaders to bring you laptops, desktops, workstations</p>
+            <span class="price">€ 319,-</span>
+            <a href="prodcutDetail.php" class="btn btn-primary">Bekijk</a> <a href="#"><i class="fas fa-shopping-cart"></i></a>
+        </div>
+      </div>
+
+      <div class="card" style="width: 18rem;">
+        <img src="image/piconeo.png" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">	Pico Neo 2</h5>
+          <p class="card-text">Pico Neo 2 Eye is een standalone headset met 6 DoF inside-out tracking. Dit maakt de headset geschikt om vrij in een ruimte rond te kunnen lopen (free-roam) zonder dat je een PC of externe sensoren nodig hebt.</p>
+            <span class="price">€ 665,50 -</span>
+            <a href="#" class="btn btn-primary">Bekijk</a> <a href="#"><i class="fas fa-shopping-cart"></i></a>
+        </div>
+      </div>
+
+      <div class="card" style="width: 18rem;">
+        <img src="image/lgr100.jpeg" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">LG R100 360 VR</h5>
+          <p class="card-text">Heb je een LG G5 en wil je de wondere wereld van virtual reality ervaren? De LG 360 VR heeft - in tegenstelling tot andere VR-brillen - een ingebouwd scherm. Dit maakt deze VR-bril een stuk lichter dan vergelijkbare modellen.</p>
+            <span class="price">€ 62,90 -</span>
+            <a href="#" class="btn btn-primary">Bekijk</a> <a href="#"><i class="fas fa-shopping-cart"></i></a>
+        </div>
+      </div>
+
+      <div class="card" style="width: 18rem;">
+        <img src="image/salora.jpeg" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">Salora Virtual Reality Hawk</h5>
+          <p class="card-text">De Salora VR HAWK is een stijlvol vormgegeven kunststof Virtual Reality bril voor smartphones van 3,5 tot 6 inch. Lens en focus kunnen op maat worden ingesteld. De voorzijde kan open geschoven worden, zodat de camera van uw telefoon vrijkomt.</p>
+            <span class="price">€ 16,95-</span>
+            <a href="#" class="btn btn-primary">Bekijk</a> <a href="#"><i class="fas fa-shopping-cart"></i></a>
+        </div>
+      </div>
+
+      <div class="card" style="width: 18rem;">
+        <img src="image/hpreverb.jpeg" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">HP Reverb G2 - met controllers</h5>
+          <p class="card-text">De HP Reverb G2 is een pcvr-headset met een zeer hoge resolutie, goed draagcomfort en compatibiliteit met zowel Windows Mixed Reality als SteamVR. De beeldscherpte van de headset is beter dan die van concurrerende modellen.</p>
+            <span class="price">€ 689,-</span>
+            <a href="#" class="btn btn-primary">Bekijk</a> <a href="#"><i class="fas fa-shopping-cart"></i></a>
+        </div>
+      </div>
+
+      <div class="card" style="width: 18rem;">
         <img src="image/FOTONAAM.png" class="card-img-top">
         <div class="card-body">
           <h5 class="card-title">TITEL-PRODUCT</h5>
@@ -59,6 +212,7 @@
             <a href="#" class="btn btn-primary">Bekijk</a> <a href="#"><i class="fas fa-shopping-cart"></i></a>
         </div>
       </div>
+      <br>
 
       <div class="card" style="width: 18rem;">
         <img src="image/FOTONAAM.png" class="card-img-top">
@@ -90,15 +244,6 @@
         </div>
       </div>
 
-      <div class="card" style="width: 18rem;">
-        <img src="image/FOTONAAM.png" class="card-img-top">
-        <div class="card-body">
-          <h5 class="card-title">TITEL-PRODUCT</h5>
-          <p class="card-text">BESCHRIJVING-PRODUCT</p>
-            <span class="price">€ 0,-</span>
-            <a href="#" class="btn btn-primary">Bekijk</a> <a href="#"><i class="fas fa-shopping-cart"></i></a>
-        </div>
-      </div>
 
       <div class="card" style="width: 18rem;">
         <img src="image/FOTONAAM.png" class="card-img-top">
@@ -113,8 +258,8 @@
       <div class="card" style="width: 18rem;">
         <img src="image/FOTONAAM.png" class="card-img-top">
         <div class="card-body">
-          <h5 class="card-title">TITEL-PRODUCT</h5>
-          <p class="card-text">BESCHRIJVING-PRODUCT</p>
+          <h5 class="card-title"></h5>
+          <p class="card-text">/p>
             <span class="price">€ 0,-</span>
             <a href="#" class="btn btn-primary">Bekijk</a> <a href="#"><i class="fas fa-shopping-cart"></i></a>
         </div>
@@ -123,8 +268,8 @@
       <div class="card" style="width: 18rem;">
         <img src="image/FOTONAAM.png" class="card-img-top">
         <div class="card-body">
-          <h5 class="card-title">TITEL-PRODUCT</h5>
-          <p class="card-text">BESCHRIJVING-PRODUCT</p>
+          <h5 class="card-title"></h5>
+          <p class="card-text"></p>
             <span class="price">€ 0,-</span>
             <a href="#" class="btn btn-primary">Bekijk</a> <a href="#"><i class="fas fa-shopping-cart"></i></a>
         </div>
@@ -133,88 +278,19 @@
       <div class="card" style="width: 18rem;">
         <img src="image/FOTONAAM.png" class="card-img-top">
         <div class="card-body">
-          <h5 class="card-title">TITEL-PRODUCT</h5>
-          <p class="card-text">BESCHRIJVING-PRODUCT</p>
+          <h5 class="card-title"></h5>
+          <p class="card-text"></p>
             <span class="price">€ 0,-</span>
             <a href="#" class="btn btn-primary">Bekijk</a> <a href="#"><i class="fas fa-shopping-cart"></i></a>
         </div>
       </div>
 
-      <div class="card" style="width: 18rem;">
-        <img src="image/FOTONAAM.png" class="card-img-top">
-        <div class="card-body">
-          <h5 class="card-title">TITEL-PRODUCT</h5>
-          <p class="card-text">BESCHRIJVING-PRODUCT</p>
-            <span class="price">€ 0,-</span>
-            <a href="#" class="btn btn-primary">Bekijk</a> <a href="#"><i class="fas fa-shopping-cart"></i></a>
-        </div>
-      </div>
 
       <div class="card" style="width: 18rem;">
         <img src="image/FOTONAAM.png" class="card-img-top">
         <div class="card-body">
-          <h5 class="card-title">TITEL-PRODUCT</h5>
-          <p class="card-text">BESCHRIJVING-PRODUCT</p>
-            <span class="price">€ 0,-</span>
-            <a href="#" class="btn btn-primary">Bekijk</a> <a href="#"><i class="fas fa-shopping-cart"></i></a>
-        </div>
-      </div>
-
-      <div class="card" style="width: 18rem;">
-        <img src="image/FOTONAAM.png" class="card-img-top">
-        <div class="card-body">
-          <h5 class="card-title">TITEL-PRODUCT</h5>
-          <p class="card-text">BESCHRIJVING-PRODUCT</p>
-            <span class="price">€ 0,-</span>
-            <a href="#" class="btn btn-primary">Bekijk</a> <a href="#"><i class="fas fa-shopping-cart"></i></a>
-        </div>
-      </div>
-
-      <div class="card" style="width: 18rem;">
-        <img src="image/FOTONAAM.png" class="card-img-top">
-        <div class="card-body">
-          <h5 class="card-title">TITEL-PRODUCT</h5>
-          <p class="card-text">BESCHRIJVING-PRODUCT</p>
-            <span class="price">€ 0,-</span>
-            <a href="#" class="btn btn-primary">Bekijk</a> <a href="#"><i class="fas fa-shopping-cart"></i></a>
-        </div>
-      </div>
-
-      <div class="card" style="width: 18rem;">
-        <img src="image/FOTONAAM.png" class="card-img-top">
-        <div class="card-body">
-          <h5 class="card-title">TITEL-PRODUCT</h5>
-          <p class="card-text">BESCHRIJVING-PRODUCT</p>
-            <span class="price">€ 0,-</span>
-            <a href="#" class="btn btn-primary">Bekijk</a> <a href="#"><i class="fas fa-shopping-cart"></i></a>
-        </div>
-      </div>
-
-      <div class="card" style="width: 18rem;">
-        <img src="image/FOTONAAM.png" class="card-img-top">
-        <div class="card-body">
-          <h5 class="card-title">TITEL-PRODUCT</h5>
-          <p class="card-text">BESCHRIJVING-PRODUCT</p>
-            <span class="price">€ 0,-</span>
-            <a href="#" class="btn btn-primary">Bekijk</a> <a href="#"><i class="fas fa-shopping-cart"></i></a>
-        </div>
-      </div>
-
-      <div class="card" style="width: 18rem;">
-        <img src="image/FOTONAAM.png" class="card-img-top">
-        <div class="card-body">
-          <h5 class="card-title">TITEL-PRODUCT</h5>
-          <p class="card-text">BESCHRIJVING-PRODUCT</p>
-            <span class="price">€ 0,-</span>
-            <a href="#" class="btn btn-primary">Bekijk</a> <a href="#"><i class="fas fa-shopping-cart"></i></a>
-        </div>
-      </div>
-
-      <div class="card" style="width: 18rem;">
-        <img src="image/FOTONAAM.png" class="card-img-top">
-        <div class="card-body">
-          <h5 class="card-title">TITEL-PRODUCT</h5>
-          <p class="card-text">BESCHRIJVING-PRODUCT</p>
+          <h5 class="card-title"></h5>
+          <p class="card-text"></p>
             <span class="price">€ 0,-</span>
             <a href="#" class="btn btn-primary">Bekijk</a> <a href="#"><i class="fas fa-shopping-cart"></i></a>
         </div>
