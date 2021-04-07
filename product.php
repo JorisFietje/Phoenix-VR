0cc76fe281bd81cf1f535cb89699d4317f7dce48
Thabit030 <57993677+Thabit030@users.noreply.github.com>
2021-04-07 11:00:30 +0200

    Update product.php

--- product.php
+++ product.php
@@ -48,13 +48,16 @@ $page = "product";
           }
         }
       } else {
-        echo "Er zijn helaas op dit moment geen beschikbare producten.";
+        //echo "No results.";
       }
       ?>
+      <br><br>
+      <hr>
+      <br><br>
 
-    </div>
 
-  </div>
+
+
 
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
   </script>
