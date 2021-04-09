bbd0aaff001f504760a0b4da8b9e861e7e99a8b3
Thabit030 <57993677+Thabit030@users.noreply.github.com>
2021-04-09 09:02:26 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,39 +1,10 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="stylesheet" type="text/css" href="css/style.css" />
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.7/css/all.css" />
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous" />
-    </head>
-    <body>
-    <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
-            <a class="navbar-brand" href="index.php">Multiversum</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+<?php include_once("incl/header.php"); ?>
+</head>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="product.php">Producten</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="contact.php">Contact</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="about.php">Over Ons</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="shopping.php">Winkelwagen</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+<body>
+  <?php include_once("incl/menu.php"); ?>
 
         <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
             <ol class="carousel-indicators">
@@ -62,9 +33,8 @@
                 <span class="sr-only">Next</span>
             </a>
         </div>
-
         <div class="container">
-        <center><h1 style="margin: 40px;">Best Verkochte VR-Brillen</h1></center>
+        <center> <h1 style="margin: 40px;">Best Verkochte VR-Brillen</h1> </center>
           <div class="row">
             
           <div class="card" style="width: 18rem; margin: 40px;">
@@ -97,7 +67,7 @@
         </div>
           </div>
         </div>
-        <footer class="bg-dark" style="marin-top: 60px;">
+        <footer class="bg-dark" style="margin-top: 60px;">
           <p>Gemaakt door Joris Fietjé en Thabit Oulali</p>
         </footer>
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
