0e6cbc52f0dfa7a7ae9c118db894c50f5e82d212
Thabit030 <57993677+Thabit030@users.noreply.github.com>
2021-04-15 01:01:40 +0200

    Update product.php

--- product.php
+++ product.php
@@ -14,7 +14,21 @@ $page = "product";
 
   <div class="container">
     <div class="row">
-    <br>
+      <br>
+      <?php
+        if(isset($_GET["added"])){
+          echo "<div class=\"alert alert-success\" role=\"alert\">Product is toegevoegd aan winkelwagen!</div>";
+        }
+
+        if(isset($_GET["failedAdd"])){
+          if($_GET["failedAdd"] == "ID"){
+            echo "<div class=\"alert alert-danger\" role=\"alert\">Product is niet toegevoegd aan winkelwage vanwege een ongeldig product ID!</div>";
+          }else{
+            echo "<div class=\"alert alert-danger\" role=\"alert\">Er is wat misgegaan tijdens het toevoegen van het product aan uw winkelwagen!</div>";
+          }
+        }
+
+      ?>
       <h2>VR Brillen:<br></h2>
       <p>
       <p>Met een VR-bril duik je in de wondere wereld van Virtual Reality! </p>
@@ -35,13 +49,16 @@ $page = "product";
       if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
           if ($row["active"] == "yes") {
+            if (strlen($row["product_name"]) > 20) {
+              $row["product_name"] = substr($row["product_name"], 0, 17) . '...';
+            }
             echo "
                 <div class=\"card\" style=\"width: 18rem;\">
                   <img src=\"" . $row["product_pic"] . "\" class=\"card-img-top\">
                 <div class=\"card-body\">
                   <h5 class=\"card-title\">" . $row["product_name"] . "</h5>
                   
-                <span class=\"price\">€" . $row["product_price"] . ",-</span>
+                <span class=\"price\">€" . $row["product_price"] . ",-</span><br>
                 <a href=\"productDetail.php?id=" . $row["id"] . "\" class=\"btn btn-primary\">Bekijk product</a>
               </div>
             </div>";
@@ -51,30 +68,24 @@ $page = "product";
         //echo "No results.";
       }
       ?>
-      <br><br>
-      <hr>
-      <br><br>
 
 
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+      </script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous">
+      </script>
 
 
-
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
-  </script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous">
-  </script>
-
-
-  <script>
-    function myFunction() {
-      var x = document.getElementById("myTopnav");
-      if (x.className === "topnav") {
-        x.className += " responsive";
-      } else {
-        x.className = "topnav";
-      }
-    }
-  </script>
+      <script>
+        function myFunction() {
+          var x = document.getElementById("myTopnav");
+          if (x.className === "topnav") {
+            x.className += " responsive";
+          } else {
+            x.className = "topnav";
+          }
+        }
+      </script>
 
 </body>
 
